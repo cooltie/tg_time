bc5ef1f3da8cf2dbfa4384596078c1b2a1714fce
nika ya <cooltie@gmail.com>
2024-09-25 18:08:20 +0300

    Initial commit

--- TG_timer/timetracking.py
+++ TG_timer/timetracking.py
@@ -15,7 +15,7 @@ SPREADSHEET_ID = '1D1QCbveZEJHjhERzPIC4_uEzbhV1DZccPo6RqjbdMso'
 # Авторизация в Google Sheets API
 scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
 creds = ServiceAccountCredentials.from_json_keyfile_name(
-    '/Users/nikayc/Dropbox/Projects/pythonProject/keys/timetracking-436217-8f446dc303b1.json', scope)
+    '/TG_timer/timetracking-436217-8f446dc303b1.json', scope)
 client = gspread.authorize(creds)
 sheet = client.open_by_key(SPREADSHEET_ID).sheet1  # Открываем первую страницу таблицы
 
